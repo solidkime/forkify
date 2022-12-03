1e981e60e287dbaf29837bcb9807839184ac9390
solidkime <dr.kime@gmail.com>
2022-12-03 21:10:15 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e38ddae4-f13e-479f-961b-260ea7434f80';
+export const MODAL_CLOSE_SEC = 2.5;
